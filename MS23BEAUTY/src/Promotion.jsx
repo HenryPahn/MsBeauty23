4e345dd82d42cc16c022c1a987c588c96a953e5d
HenryPahn <thoang0303@gmail.com>
2025-04-02 11:17:08 -0400

    Add Promotion

--- MS23BEAUTY/src/Promotion.jsx
+++ MS23BEAUTY/src/Promotion.jsx
@@ -6,27 +6,41 @@ export default function Promotion() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex-grow flex flex-col justify-center items-center px-8 py-12 mt-20 md:mt-24 space-y-8">
-        <div className="flex flex-col items-center text-center">
+      <div className="flex flex-col items-center mt-16 md:mt-20 lg:mt-24">
+        <div className="flex flex-col items-center text-center mt-6 mb-6 md:mt-10 md:mb-10">
+          <h1 className="text-4xl mb-4 md:text-5xl md:mb-4 font-Abril_Fatface">Free Delux Pedicure!</h1>
+          <video className="md:w-8/12 2xl:w-4/12 pl-8 pr-8" controls autoPlay muted>
+            <source src="/promotions/DeluxPedicure.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+
+          <img
+            src="/promotions/AprilPromotion.png"
+            alt="Winter 2025 Promotion"
+            className="md:w-8/12 2xl:w-4/12 lg:mt-7 pl-8 pr-8"
+          />
+        </div>
+
+        <div className="flex flex-col items-center text-center mt-6 mb-6 md:mt-10 md:mb-10">
           <h1 className="text-4xl mb-4 md:text-5xl md:mb-4 font-Abril_Fatface">Unwrap the Mystery!</h1>
           <img
             src="/promotions/Winter2025Promotion.png"
             alt="Winter 2025 Promotion"
-            className="rounded-lg shadow-md"
+            className="2xl:w-10/12 pl-8 pr-8"
           />
         </div>
 
-        <div className="flex flex-col items-center text-center pt-28">
+        <div className="flex flex-col items-center text-center mt-6 mb-6 md:mt-10 md:mb-10">
           <h1 className="text-4xl mb-4 md:text-5xl md:mb-4 font-Abril_Fatface">Exclusive Discounts on Eyelash Services!</h1>
           <img
             src="/promotions/EyelashPromotion.png"
             alt="Winter 2025 Promotion"
-            className="rounded-lg shadow-md size-5/12"
+            className="md:w-8/12 2xl:w-5/12 pl-8 pr-8"
           />
         </div>
       </div>
-      
-      <Warning/>
+
+      <Warning />
       <Footer />
     </div>
   )
